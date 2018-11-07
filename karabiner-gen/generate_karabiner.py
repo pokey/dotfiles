78c238ee7702273add8588556f28e8cb1bb9e1a0
Pokey Rule <pokey.rule@globality.com>
2018-11-07 22:16:21 +0000

    Add UK keyboard mappings

--- karabiner-gen/generate_karabiner.py
+++ karabiner-gen/generate_karabiner.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python
 import json
+import os
 
 import yaml
 
+UK_TO_US_MODIFICATIONS = {
+    'return_or_enter': 'backslash',
+    'backslash': 'return_or_enter',
+    'non_us_backslash': 'grave_accent_and_tilde',
+}
+
+
+config_yaml = os.path.expanduser('~/.config/pokey/config.yaml')
+if os.path.exists(config_yaml):
+    with open(config_yaml) as f:
+        pokey_config = yaml.load(f)
+else:
+    pokey_config = dict()
 
 with open('karabiner.yaml') as f:
     complex_modifications_input = yaml.load(f)
@@ -30,6 +44,16 @@ rule = (
     config['profiles'][0]['complex_modifications']['rules'][0]
 )
 rule['manipulators'] = complex_modifications + rule['manipulators']
+
+simple_modifications = config['profiles'][0]['simple_modifications']
+if pokey_config.get('uk_to_us_keyboard'):
+    simple_modifications += [
+        {
+            "from": {"key_code": key},
+            "to": {"key_code": value}
+        }
+        for key, value in UK_TO_US_MODIFICATIONS.items()
+    ]
 
 out = json.dumps(
     config,
