aa97eac2b65327812fd458ff8b0486a36f0e1f4e
Pokey Rule <755842+pokey@users.noreply.github.com>
2024-07-19 13:52:58 +0100

    go setup

--- dotFiles/sh/commands.sh
+++ dotFiles/sh/commands.sh
@@ -11,7 +11,7 @@ v() {
 }
 
 # list git branches
-go() {
+g() {
   local branches branch out key
   branches=$(git branch -vv --color --sort=-committerdate)
   out=$(echo "$branches" | fzf-tmux --ansi -n 1 +m -0 -1 -q "$1" --expect=ctrl-d)
